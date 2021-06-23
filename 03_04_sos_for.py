7be5aa2d494cf00d6e05f92f254baa5c462d2629
simonmonk <evilgeniusauthor@gmail.com>
2021-06-23 15:23:19 +0100

    minor fixes

--- 03_04_sos_for.py
+++ 03_04_sos_for.py
@@ -4,12 +4,14 @@ from utime import sleep
 led = Pin(25, Pin.OUT)
 
 while True:
+    # This loop does 3 dots (S)
     for x in range(1, 4):
         led.on()
         sleep(0.2)
         led.off()
         sleep(0.2)
-    sleep(0.4)
+    sleep(0.4) # Gap between S and O
+    # This loop does 3 dashes (O)
     for x in range(1, 4):
         led.on()
         sleep(0.6)
--- 05_01_sos_list.py
+++ 05_01_sos_list.py
@@ -3,7 +3,7 @@ from utime import sleep
 
 led = Pin(25, Pin.OUT)
 
-delays = [0.2, 0.2, 0.2, 0.6, 0.6, 0.6];
+delays = [0.2, 0.2, 0.2, 0.6, 0.6, 0.6]
 
 while True:
     for delay in delays:
